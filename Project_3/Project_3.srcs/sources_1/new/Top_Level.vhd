4a187c2f9fda3fa8bf91152bce6227d3ecda3248
julion <julionrowland@gmail.com>
2025-11-05 11:56:48 -0500

    top level, shift left, and block diagram updates

--- Project_3/Project_3.srcs/sources_1/new/Top_Level.vhd
+++ Project_3/Project_3.srcs/sources_1/new/Top_Level.vhd
@@ -20,18 +20,22 @@ architecture Behavioral of Top_Level is
     signal PCIn, PCOut : unsigned(15 downto 0);
     signal adderOut : unsigned(15 downto 0);
     signal instrOut : unsigned(15 downto 0);
-    signal Branch, MemtoReg, memWrite, memRead, ALUSrc, RegWrite : std_logic;
-    signal RegDst, ALUOp : unsigned(15 downto 0);
+    signal mux0Out : unsigned(3 downto 0);
+    singal shift1Out : unsiged(15 downto 0) := X"0000";
+    signal regDst, ALUSrc, memtoReg, regWrite, memRead, memWrite, branch, jump : std_logic;
+    signal ALUOp : unsigned(3 downto 0);
     signal readData1, readData2 : unsigned(15 downto 0);
     signal signExtendOut : unsigned(15 downto 0);
     signal mux1Out : unsigned(15 downto 0);
-    signal shiftOut : unsigned(15 downto 0);
+    signal ALUControlOut : unsigned(3 downto 0);
+    signal shift1Out : unsigned(15 downto 0);
     signal Zero : std_logic;
     signal ALU0Out : unsigned(15 downto 0);
     signal ALU1Out : unsigned(15 downto 0);
     signal andOut : std_logic;
     signal dataMemOut : unsigned(15 downto 0);
-    signal mux3Out : unsigned(15 downto 0);-- := RegWr;
+    signal mux2Out : unsigned(15 downto 0);
+    signal mux4Out : unsigned(15 downto 0);-- := RegWr;
     
     component ProgramCounter -- Component: Program Counter
         generic ( N : integer := 16 );
@@ -46,28 +50,43 @@ architecture Behavioral of Top_Level is
     component PCAdder -- Component: Program Adder
         generic ( N : integer := 16 );
             port (
-        pc_in  : in  unsigned(N-1 downto 0);
-        pc_out : out unsigned(N-1 downto 0)
-    );
+                pc_in  : in  unsigned(N-1 downto 0);
+                pc_out : out unsigned(N-1 downto 0)
+            );
     end component;
     
     component InstructionMemory -- Component: Instruction Memory
         generic ( N : integer := 16 );
             port (
-        clk   : in  std_logic;
-        addr  : in  unsigned(N-1 downto 0);
-        instr : out unsigned(N-1 downto 0)
-    );
+                clk   : in  std_logic;
+                addr  : in  unsigned(N-1 downto 0);
+                instr : out unsigned(N-1 downto 0)
+            );
+    end component;
+    
+    component ControlUnit -- Component: Instruction Memory
+        port (
+            instr     : in  unsigned(15 downto 12);
+            RegDst    : out std_logic;
+            ALUSrc    : out std_logic;
+            MemToReg  : out std_logic;
+            RegWrite  : out std_logic;
+            MemRead   : out std_logic;
+            MemWrite  : out std_logic;
+            Branch    : out std_logic;
+            Jump      : out std_logic;
+            ALUOp     : out unsigned(3 downto 0)
+        );
     end component;
     
     component MUX2to1 -- Component: MUX
-        generic ( N : integer := 16 );
-            port (
-        A   : in  unsigned(N-1 downto 0);
-        B   : in  unsigned(N-1 downto 0);
-        Sel : in  std_logic;
-        Y   : out unsigned(N-1 downto 0)
-    );
+        generic(N : integer := 16);
+        port (
+            A   : in  unsigned(N-1 downto 0);
+            B   : in  unsigned(N-1 downto 0);
+            Sel : in  std_logic;
+            Y   : out unsigned(N-1 downto 0)
+        );
     end component;
     
     component Registers -- Component: Registers
@@ -83,17 +102,26 @@ architecture Behavioral of Top_Level is
     component SignExtend -- Component: Sign Extender
         generic ( N : integer := 16 );
             port (
-        In6  : in  unsigned(5 downto 0);
-        Out16 : out unsigned(N-1 downto 0)
-    );
+                In6  : in  unsigned(5 downto 0);
+                Out16 : out unsigned(N-1 downto 0)
+            );
     end component;
     
-    component ShiftLeft2 -- Component: Left shift by 2
-        generic ( N : integer := 16 );
+    component ALUControlUnit -- Component: Sign Extender
             port (
-        data_in  : in  unsigned(N-1 downto 0);
-        data_out : out unsigned(N-1 downto 0)
-    );
+                ALUOp  : in  unsigned(3 downto 0); ---<<--- might need to be larger?
+                func   : in unsigned(5 downto 0); 
+                ALUControlOut : out unsigned(3 downto 0)
+            );
+    end component;
+    
+    component ShiftLeft -- Component: Left shift by 2
+        generic ( N : integer := 16;
+                  S : integer := 2);
+            port (
+                data_in  : in  unsigned(N-1 downto 0);
+                data_out : out unsigned(N-1 downto 0)
+            );
     end component;
     
     component ALU -- Component: ALU
@@ -108,14 +136,14 @@ architecture Behavioral of Top_Level is
     
     component DataMemory -- Component: Data Memory
         generic ( N : integer := 16 );
-            port (
-        clk      : in  std_logic;
-        MemRead  : in  std_logic;
-        MemWrite : in  std_logic;
-        Address  : in  unsigned(N-1 downto 0);
-        WriteData: in  unsigned(N-1 downto 0);
-        ReadData : out unsigned(N-1 downto 0)
-    );
+        port (
+            clk      : in  std_logic;
+            MemRead  : in  std_logic;
+            MemWrite : in  std_logic;
+            Address  : in  unsigned(N-1 downto 0);
+            WriteData: in  unsigned(N-1 downto 0);
+            ReadData : out unsigned(N-1 downto 0)
+        );
     end component;
 
 begin
@@ -139,15 +167,46 @@ begin
             addr => PCOut,
             instr => instrOut
         );
-
+    
+    LSHIFT0 : ShiftLeft
+        generic map(N => 12;
+                    S => 1);
+        port map(
+            data_in => instrout(11 downto 0),
+            data_out => shift0Out(11 downto 0)
+        );
+    
+    Control : ControlUnit
+        port map(
+            instr => instrOut(15 downto 12),
+            RegDst => regDst,
+            ALUSrc => ALUSrc,
+            MemToReg => memToReg,
+            RegWrite => regWrite,
+            MemRead => memRead,
+            MemWrite => memWrite,
+            Branch => branch
+            Jump => jump,
+            ALUOp => ALUoP
+        );
+    
+    MUX0 : Mux2to1-- MUX0 RESERVED FOR register input
+        generic map(N => 3)
+        port map(
+            A => instrOut(8 downto 6),
+            B => instrOut(5 downto 3),
+            Sel => ALUSrc,
+            Y => mux1Out
+        );
+    
     RF: Registers --My Register File
         port map (
             clk   => clk,
             RegWr => RegWr,
             Ra    => instrOut(11 downto 9),
             Rb    => instrOut(8 downto 6),
-            Rw    => instrOut(5 downto 3),
-            busW  => mux3Out,
+            Rw    => mux1Out,
+            busW  => mux4Out,
             busA  => readData1,
             busB  => readData2
         );
@@ -165,11 +224,18 @@ begin
             Sel => ALUSrc,
             Y => mux1Out
         );
-        
-    LSHIFT : ShiftLeft2
+    
+    ALUControl : ALUControlUnit
+        port map(
+            ALUOp => ALUOp,
+            func => instrOut(5 downto 0),
+            ALUControlOut => ALUControlOut
+        );
+    
+    LSHIFT1 : ShiftLeft
         port map(
             data_in => signExtendOut,
-            data_out => shiftOut
+            data_out => shift1Out
         );
    
     ALU0: ALU --My Alu
@@ -177,7 +243,7 @@ begin
         port map (
             A        => readData1,
             B        => mux1Out,
-            ALUctr   => ALUctr,
+            ALUctr   => ALUControlOut,
             Result   => ALU0Out,
             Zero     => Zero,
             Overflow => open,
@@ -213,15 +279,23 @@ begin
             A => adderOut,
             B => ALU1Out,
             Sel => andOut,
-            Y => PCIn
+            Y => mux2Out
         );
     
     MUX3 : Mux2to1
         port map(
+            A => shift0Out,
+            B => mux2Out,
+            Sel => jump,
+            Y => PCIn
+        );
+    
+    MUX4 : Mux2to1
+        port map(
             A => dataMemOut,
             B => ALU0Out,
             Sel => MemtoReg,
-            Y => mux3Out
+            Y => mux4Out
         );
     
     Result <= mux3Out; --Final Result
--- Project_3/Project_3.srcs/sources_1/new/left_shift.vhd
+++ Project_3/Project_3.srcs/sources_1/new/left_shift.vhd
@@ -2,18 +2,19 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity ShiftLeft2 is
+entity ShiftLeft is
     generic (
-        WIDTH_IN  : integer := 16; 
-        WIDTH_OUT : integer := 16   
+        WIDTH_IN  : integer := 16;
+        WIDTH_OUT : integer := 16;
+        SHIFT_SIZE : integer := 2
     );
     port (
         data_in  : in  unsigned(WIDTH_IN-1 downto 0);
         data_out : out unsigned(WIDTH_OUT-1 downto 0)
     );
-end entity ShiftLeft2;
+end entity ShiftLeft;
 
-architecture Behavioral of ShiftLeft2 is
+architecture Behavioral of ShiftLeft is
 begin
-    data_out <= data_in(WIDTH_IN-3 downto 0) & "00";
+    data_out <= data_in(WIDTH_IN-(SHIFT_SIZE+1) downto 0) & "00";
 end architecture Behavioral;
