f34551162a1c3cd0fe2a6b244e7edc71c4c09415
Dustin Trimmer <my1dustin@gmail.com>
2025-11-05 14:38:10 -0500

    added ControlUnitTB and ALUControlTB

---
+++ Project_3/Project_3.srcs/sim_1/new/ALUControl_TB.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/05/2025 02:37:38 PM
+-- Design Name: 
+-- Module Name: ALUControl_TB - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ALUControl_TB is
+--  Port ( );
+end ALUControl_TB;
+
+architecture Behavioral of ALUControl_TB is
+
+begin
+
+
+end Behavioral;
